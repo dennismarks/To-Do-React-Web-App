da3d1874d82ba29e4c30f7d3181cbdd60e448a04
Dennis M <dennismarkshome@gmail.com>
2019-07-08 10:00:00 -0600

    add checkbox and style

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,10 +5,23 @@ import PropTypes from 'prop-types';
 
 
 export class TodoItem extends Component {
+
+    getStyle = () => {
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc solid',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+        }
+    }
+
     render() {
         return (
-            <div>
-                <p>{this.props.todo.title}</p>
+            <div style={this.getStyle()}> 
+                <p>
+                    <input style={{marginRight: '6px'}} type='checkbox' />
+                    {this.props.todo.title}
+                </p>
             </div>
         )
     }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -11,7 +11,7 @@ class Todos extends Component {
 	}
 }
 
-// PropTypes
+// PropTypes - validation for props that a comp should have
 Todos.propTypes = {
 	todos: PropTypes.array.isRequired
 }
