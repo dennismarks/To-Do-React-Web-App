bd654870c918e143da49137cde973d6138903f0c
Dennis M <dennismarkshome@gmail.com>
2019-07-09 10:30:00 -0600

    delete item when click the remove button

--- src/App.js
+++ src/App.js
@@ -24,21 +24,26 @@ class App extends Component {
 		]
 	}
 
+	// Toggle complete
 	markComplete = (id) => {
 		this.setState({todos: this.state.todos.map(todo => {
-			if (todo.id == id) {
+			if (todo.id === id) {
 				todo.completed = !todo.completed
 			}
 			return todo;
-		}) });
+		})});
 	}
 
+	// Delete Todo
+	delTodo = (id) => {
+		this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+	}
 
 	render() {
 		console.log(this.state.todos)
 		return (
 		  <div className="App">
-			<Todos todos={this.state.todos} markComplete={this.markComplete}/>
+			<Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
 		  </div>
 		);
 	  }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -24,6 +24,7 @@ export class TodoItem extends Component {
                 <p>
                     <input style={{marginRight: '6px'}} type='checkbox' onChange={this.props.markComplete.bind(this, id)}/>
                     {title}
+                    <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>X</button>
                 </p>
             </div>
         )
@@ -33,6 +34,16 @@ export class TodoItem extends Component {
 // PropTypes - validation for props that a comp should have
 TodoItem.propTypes = {
 	todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 8px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
 }
 
 export default TodoItem
