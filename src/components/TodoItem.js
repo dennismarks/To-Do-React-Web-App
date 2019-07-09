f8ce0426dd7452a0a8be9252dcae37c71a0de52f
Dennis M <dennismarkshome@gmail.com>
2019-07-09 10:00:00 -0600

    update the state of the item when check the box

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -16,11 +16,14 @@ export class TodoItem extends Component {
     }
 
     render() {
+
+        const { id, title } = this.props.todo
+
         return (
             <div style={this.getStyle()}> 
                 <p>
-                    <input style={{marginRight: '6px'}} type='checkbox' />
-                    {this.props.todo.title}
+                    <input style={{marginRight: '6px'}} type='checkbox' onChange={this.props.markComplete.bind(this, id)}/>
+                    {title}
                 </p>
             </div>
         )
