e2c212c909a1c2b579656e98a3880f9c7b4fb701
Dennis M <dennismarkshome@gmail.com>
2019-07-13 07:15:00 -0600

    add prop types

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -17,7 +17,9 @@ class Todos extends Component {
 
 // PropTypes - validation for props that a comp should have
 Todos.propTypes = {
-  todos: PropTypes.array.isRequired
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
 };
 
 export default Todos;
